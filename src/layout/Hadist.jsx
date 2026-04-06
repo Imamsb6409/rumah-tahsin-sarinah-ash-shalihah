4960848c64b5d93b2eeee22beb607578e46896bd
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-04-06 10:41:22 +0700

    yeah gtu lah

---
+++ src/layout/Hadist.jsx
@@ -0,0 +1,7 @@
+export default function Hadist() {
+  return (
+    <div className="hadist">
+      <h1>Hadist</h1>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
-  import { StrictMode } from 'react'
-  import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-  import {Route, Routes,BrowserRouter} from 'react-router';
+import { Route, Routes, BrowserRouter } from "react-router";
 
-  import './index.css'
-  import App from './App.jsx'
-  import Main from './layout/Main.jsx';
+import "./index.css";
+import App from "./App.jsx";
+import Main from "./layout/Main.jsx";
+import Hadist from "./layout/Hadist.jsx";
 
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} >
-          <Route path='/' element={<Main />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </StrictMode>,
-  )
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/hadist" element={<Hadist />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
