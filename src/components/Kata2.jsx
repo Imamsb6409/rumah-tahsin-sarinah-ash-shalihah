57a8420997d65e8802ff03d8ae253d0e21d5cf4c
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-04-08 11:00:01 +0700

    update hampir final 4.6

--- src/components/Kata2.jsx
+++ src/components/Kata2.jsx
@@ -8,12 +8,13 @@ const kata2 = [
 
 export default function Kata2() {
   return (
-    <div className="min-h-max items-center justify-center bg-primary pt-22 pb-12 text-secondary">
+    <div className="min-h-max flex flex-col items-center justify-center bg-primary pt-22 pb-12 text-secondary">
       <h3 className=" text-xl md:text-4xl font-semibold ml-4 text-center font-quicksand">
       {kata2[0].arabic}
       </h3>
       <p className="text-md md:text-lg text-center mt-4">{kata2[0].arti}</p>
       <p className=" text-sm md:text-md text-center mt-2 italic">{kata2[0].riwayat}</p>
+      <div className="w-[70%] mt-2"><hr /></div>
     </div>
   );
 }
